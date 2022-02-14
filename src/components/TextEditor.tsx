20769249f9b3511b2c2058e265baebfae493a9bd
jonatas araujo <jonatasaraujofx@gmail.com>
2022-02-14 11:58:22 -0300

    feat (WIP): replace styled from editor to styled-jsx

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,72 +1,72 @@
 import { useRef, useEffect } from "react";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
 import * as ReadingEase from './../libs/readability/ReadingEase';
 
 
-const EditorDiv = styled.div`
-    font-family: 'Merriweather', serif;
-    font-size: 16px;
-    font-weight: 400;
-    color: $black;
-    line-height: 1.75;
+// const EditorDiv = styled.div`
+//     font-family: 'Merriweather', serif;
+//     font-size: 16px;
+//     font-weight: 400;
+//     color: $black;
+//     line-height: 1.75;
 
-    width: 99% !important;
-    min-width: 300px;
+//     width: 99% !important;
+//     min-width: 300px;
 
-    height: 70vh;
+//     height: 70vh;
 
-    background-color: transparent;
-    overflow: hidden;
-    overflow-y: visible;
-    resize: none;
-    border: none;
+//     background-color: transparent;
+//     overflow: hidden;
+//     overflow-y: visible;
+//     resize: none;
+//     border: none;
 
-    padding: 0 16px 0 0;
+//     padding: 0 16px 0 0;
 
-    figure{
-        div{
-            margin: 0 !important;
-            padding: 0 !important;
-        }
-    }
+//     figure{
+//         div{
+//             margin: 0 !important;
+//             padding: 0 !important;
+//         }
+//     }
 
-    img, figure{
-        width: 90% !important;
-        height: auto;
-        margin: 0 auto !important;
-        padding: 0 !important;
-    }
-    &:focus{
-        outline: none !important;
-        border: none;
-    }
-    &::-webkit-scrollbar{
-        width: 10px;
-    }
-    &::-webkit-scrollbar-track{
-        background: #f1f1f1; 
-    }
-    &::-webkit-scrollbar-thumb{
-        background: #888; 
-    }
-    &::-webkit-scrollbar-thumb:hover {
-        background: #555; 
-        cursor: pointer; 
-    }
-    @media (min-width: 1200px) {
-        font-size: 18px;
-        padding-bottom: 0;
-        border: none;
-    }
-    &:empty:before {
-        content: attr(placeholder);
-        position: absolute;
-        color: gray;
-        background-color: transparent;
-    }
-`
+//     img, figure{
+//         width: 90% !important;
+//         height: auto;
+//         margin: 0 auto !important;
+//         padding: 0 !important;
+//     }
+//     &:focus{
+//         outline: none !important;
+//         border: none;
+//     }
+//     &::-webkit-scrollbar{
+//         width: 10px;
+//     }
+//     &::-webkit-scrollbar-track{
+//         background: #f1f1f1; 
+//     }
+//     &::-webkit-scrollbar-thumb{
+//         background: #888; 
+//     }
+//     &::-webkit-scrollbar-thumb:hover {
+//         background: #555; 
+//         cursor: pointer; 
+//     }
+//     @media (min-width: 1200px) {
+//         font-size: 18px;
+//         padding-bottom: 0;
+//         border: none;
+//     }
+//     &:empty:before {
+//         content: attr(placeholder);
+//         position: absolute;
+//         color: gray;
+//         background-color: transparent;
+//     }
+// `
 
 type OnChangePropsType = {
     html: string,
@@ -205,7 +205,78 @@ export default function Component(props: ComponentPropsType) {
         handleEditorChange();
     }, [props.html]);
     
-    return (<EditorDiv placeholder="Digite teu texto..." dangerouslySetInnerHTML={{
+    return (
+        <>
+        {/* <EditorDiv placeholder="Digite teu texto..." dangerouslySetInnerHTML={{
         __html: props.html
-    }} contentEditable={true} ref={editorRef} onInput={handleEditorChange} className={props.className ? props.className : ""} />)
+        }} contentEditable={true} ref={editorRef} onInput={handleEditorChange} className={props.className ? props.className : ""} /> */}
+        <div placeholder="Digite teu texto..." dangerouslySetInnerHTML={{
+        __html: props.html
+        }} contentEditable={true} ref={editorRef} onInput={handleEditorChange} className={"texteditor "+props.className ? props.className : ""}></div>
+
+        <style jsx>{`
+        div {
+            font-family: 'Merriweather', serif;
+            font-size: 16px;
+            font-weight: 400;
+            color: $black;
+            line-height: 1.75;
+
+            width: 99% !important;
+            min-width: 300px;
+
+            height: 70vh;
+
+            background-color: transparent;
+            overflow: hidden;
+            overflow-y: visible;
+            resize: none;
+            border: none;
+
+            padding: 0 16px 0 0;
+        }
+        div figure div{
+            margin: 0 !important;
+            padding: 0 !important;
+        }
+
+        div img, div figure{
+            width: 90% !important;
+            height: auto;
+            margin: 0 auto !important;
+            padding: 0 !important;
+        }
+        div:focus{
+            outline: none !important;
+            border: none;
+        }
+        div::-webkit-scrollbar{
+            width: 10px;
+        }
+        div::-webkit-scrollbar-track{
+            background: #f1f1f1; 
+        }
+        div::-webkit-scrollbar-thumb{
+            background: #888; 
+        }
+        div::-webkit-scrollbar-thumb:hover {
+            background: #555; 
+            cursor: pointer; 
+        }
+        @media (min-width: 1200px) {
+            div{
+                font-size: 18px;
+                padding-bottom: 0;
+                border: none;
+            }
+        }
+        div:empty:before {
+            content: attr(placeholder);
+            position: absolute;
+            color: gray;
+            background-color: transparent;
+        }
+      `}</style>
+        </>
+    )
 }
