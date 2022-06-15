0e570b8ae81a6a89cceee286cb73358603cb5239
Jônatas Araújo <20614119+jnaraujo@users.noreply.github.com>
2022-06-15 10:37:35 -0300

    feat: add preact

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   target: "experimental-serverless-trace",
   swcMinify: true,
-  generateBuildId: () => 'build',
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+  generateBuildId: () => "build",
   experimental: {
     styledComponents: true,
-  }
-}
+  },
+};
