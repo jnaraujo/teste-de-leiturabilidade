db99f2b597164c043147dd58ac8f9c189bb42281
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-12 00:14:31 -0300

    fix: build

--- next.config.js
+++ next.config.js
@@ -1,18 +1,26 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// webpack: (config, { dev, isServer }) => {
+//   if (!dev && !isServer) {
+//     Object.assign(config.resolve.alias, {
+//       react: "preact/compat",
+//       "react-dom/test-utils": "preact/test-utils",
+//       "react-dom": "preact/compat",
+//     });
+//   }
+//   return config;
+// },
+
+const nextConfig = {
+  target: "experimental-serverless-trace",
   swcMinify: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
   generateBuildId: () => "build",
   pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
   compiler: {
     styledComponents: true,
   },
 };
+
+module.exports = withBundleAnalyzer(nextConfig);
