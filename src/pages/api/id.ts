c649d9090bbdf3a011601c17a0e44127976c30d5
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-11 21:05:14 -0300

    .

--- src/pages/api/id.ts
+++ src/pages/api/id.ts
@@ -1,30 +1,30 @@
-/* eslint-disable no-undef */
-import { getPostById, getPostRawById } from "@services/blog";
+// /* eslint-disable no-undef */
+// import { getPostById, getPostRawById } from "@services/blog";
 
-export default async function handler(req, res) {
-  const { id } = req.query;
+// export default async function handler(req, res) {
+//   const { id } = req.query;
 
-  const postId = String(id).toLowerCase().replace(/-/g, " ");
+//   const postId = String(id).toLowerCase().replace(/-/g, " ");
 
-  try {
-    const postData = await getPostById(postId);
+//   try {
+//     const postData = await getPostById(postId);
 
-    const postRaw = await getPostRawById(postData.id);
+//     const postRaw = await getPostRawById(postData.id);
 
-    res.status(200).json({
-      data: postRaw,
-      info: {
-        id: postData.id,
-        publishedAt: postData.Published,
-        status: postData.Status,
-        tags: postData.Tags,
-        title: postData.Name,
-        body: postData.Description,
-      },
-    });
-  } catch (err) {
-    res.json({
-      error: err,
-    });
-  }
-}
+//     res.status(200).json({
+//       data: postRaw,
+//       info: {
+//         id: postData.id,
+//         publishedAt: postData.Published,
+//         status: postData.Status,
+//         tags: postData.Tags,
+//         title: postData.Name,
+//         body: postData.Description,
+//       },
+//     });
+//   } catch (err) {
+//     res.json({
+//       error: err,
+//     });
+//   }
+// }
