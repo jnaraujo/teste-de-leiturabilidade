0e3a0691297a14165980751cc0db5711aeca09c5
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-12 21:02:07 -0300

    feat: create posts endpoint

--- src/pages/api/blog/posts.tsx
+++ src/pages/api/blog/posts.tsx
@@ -1,29 +1,11 @@
-import { getPost, getPosts } from "../../../network/blog";
+import { getPosts } from "../../../network/blog";
 
 export default async function Posts(req, res) {
   const posts = await getPosts();
-  const paypack = [];
-  const promises = [];
-
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
-  );
-
+  const urls = [];
   for (let i = 0; i < posts.length; i += 1) {
-    promises.push(getPost(posts[i].slug));
+    urls.push(`${req?.headers?.host}/${posts[i].slug}`);
   }
 
-  const result = await Promise.all(promises);
-
-  for (let i = 0; i < posts.length; i += 1) {
-    paypack.push({
-      title: result[i].meta.title,
-      slug: posts[i].slug,
-      date: result[i].meta.date,
-      description: result[i].meta.description,
-    });
-  }
-
-  res.json(paypack);
+  res.status(200).send(urls.join("\n"));
 }
