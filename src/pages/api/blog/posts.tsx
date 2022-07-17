0b3826e0b35f2e764fc5c770f383f96c990a14e4
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-17 16:14:06 -0300

    fix: blog api

--- src/pages/api/blog/posts.tsx
+++ src/pages/api/blog/posts.tsx
@@ -4,7 +4,7 @@ export default async function Posts(req, res) {
   const posts = await getPosts();
   const urls = [];
   for (let i = 0; i < posts.length; i += 1) {
-    urls.push(`${req?.headers?.host}/${posts[i].slug}`);
+    urls.push(`https://${req?.headers?.host}/${posts[i].slug}`);
   }
 
   res.status(200).send(urls.join("\n"));
