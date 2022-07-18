01a3b3e9f30cd265c59328a160583c88f9e4556b
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-17 21:12:10 -0300

    fix: icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel='shortcut icon' type='image/x-icon' href="/favicon.ico" />
+          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
