a2f9421f67d98dce218c65537b01201a309851cf
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-17 21:12:01 -0300

    feat: add icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel='shortcut icon' type='image/x-icon' href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
