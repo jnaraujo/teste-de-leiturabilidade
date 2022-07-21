6f120c83ebcc1a9aa0c20e7596af39d8e8bea3f0
Jonatas A <jonatasaraujofx@gmail.com>
2022-07-21 20:03:39 -0300

    update: add clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,12 +11,19 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;500&display=swap"
             rel="stylesheet"
           />
 
@@ -25,6 +32,7 @@ export default class MyDocument extends Document {
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
           />
           <script
+            type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
@@ -33,6 +41,19 @@ export default class MyDocument extends Document {
             gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
               page_path: window.location.pathname,
             });
+          `,
+            }}
+          />
+
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_ID});
           `,
             }}
           />
