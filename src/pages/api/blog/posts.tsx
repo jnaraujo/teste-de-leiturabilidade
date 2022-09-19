19242d3d3e1cf76635e76c0998dd733671c51007
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-19 11:32:56 -0300

    🔥 chore: add BlogService to posts api

--- src/pages/api/blog/posts.tsx
+++ src/pages/api/blog/posts.tsx
@@ -1,7 +1,9 @@
-import { getPosts } from "../../../network/blog";
+import { BlogService } from "src/services/BlogService";
 
-export default async function Posts(req, res) {
-  const posts = await getPosts();
+const blogService = new BlogService(process.env.NOTION_BLOG_ID as string);
+
+export default async function Posts(req: any, res: any) {
+  const posts = await blogService.getBlogPosts();
   const urls = [];
   for (let i = 0; i < posts.length; i += 1) {
     urls.push(`https://www.leitura.jnaraujo.com/blog/${posts[i].slug}`);
