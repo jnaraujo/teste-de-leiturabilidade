0aa7622153c20128960e70692d6cb6342083b3de
Jonatas A <jonatasaraujofx@gmail.com>
2022-09-19 11:45:14 -0300

    🏷️ Add NextConfig typos

--- next.config.js
+++ next.config.js
@@ -4,16 +4,17 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   swcMinify: true,
-
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       // eslint-disable-next-line no-param-reassign
       config.optimization.minimizer = [];
       config.optimization.minimizer.push(new TerserPlugin({ parallel: true }));
     }
-
     return config;
   },
   pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
