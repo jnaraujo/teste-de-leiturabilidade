d2e97a99c8867e244d4f806ed3692a32473024a7
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-26 20:17:34 -0300

    ✨ feat(WIP): Add darkTheme to theme styles

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -47,4 +47,33 @@ const lightTheme: DefaultTheme = {
   },
 };
 
-export { lightTheme };
+const darkTheme: DefaultTheme = {
+  colors: {
+    primary: "#1a1a1a",
+    onPrimary: "#fafafa",
+
+    secondary: "#7B61FF",
+    onSecondary: "#fafafa",
+
+    surface: "#212529",
+    onSurface: "#fafafa",
+    onSurfaceSecondary: "#cccccc",
+
+    background: "#212529",
+    onBackground: "#6c757d",
+
+    semantic: {
+      link: "#00be8b",
+      error: "#ef233c",
+    },
+  },
+  breakpoints: {
+    xs: "0px",
+    sm: "600px",
+    md: "960px",
+    lg: "1280px",
+    xl: "1920px",
+  },
+};
+
+export { lightTheme, darkTheme };
