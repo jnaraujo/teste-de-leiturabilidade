314fbb8015e24a233cb4ec73d001cb10006f5081
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-09-26 17:51:06 -0300

    🔥 chore: Improved storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +8,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+  nextRouter: {
+    Provider: RouterContext.Provider,
+  },
+};
