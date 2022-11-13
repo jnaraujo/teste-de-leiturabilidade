892fa23648908b91697da2bb3e6f4c5086475be8
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-11-13 13:27:53 -0300

    🎨 style: improved code formatting

--- src/components/TextEditor/Toolbar/styles.tsx
+++ src/components/TextEditor/Toolbar/styles.tsx
@@ -2,30 +2,28 @@ import styled from "styled-components";
 
 export const Toolbar = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: ${({ theme }) => theme.colors.onPrimary};
 
   width: 100%;
   height: fit-content;
 
-  align-items: center;
-  justify-content: center;
-
+  padding: 8px 0;
   margin-bottom: 16px;
 
-  padding: 8px 0;
   border-bottom: 1px solid;
   border-top: 1px solid;
   border-color: ${(props) => props.theme.colors.onBackground};
 
   .group {
     display: flex;
-
     align-items: center;
-
-    padding: 0 16px;
 
     height: 30px;
 
+    padding: 0 16px;
     gap: 8px;
 
     &:not(:first-child) {
@@ -37,24 +35,19 @@ export const Toolbar = styled.div`
   }
   @media (max-width: 720px) {
     z-index: 100;
+
     position: fixed;
     bottom: 0;
     left: 0;
     right: 0;
 
     justify-content: left;
-
     width: 100vw;
-
-    /* padding: 8px 32px; */
-
     box-shadow: 0px 0px 15px 5px rgba(0, 0, 0, 0.31);
 
     overflow-x: scroll;
 
-    /* flex-wrap: wrap; */
     border: none;
-
     margin-bottom: 0;
 
     .group {
