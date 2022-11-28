7b1fb72445bb75ec31ab20afb500608124e2fb94
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-11-28 18:13:28 -0300

    ✨ feat: add reading store

---
+++ src/store/readingStore.ts
@@ -0,0 +1,23 @@
+import create from "zustand";
+
+export interface IEase {
+  index: number;
+  syllables: number;
+  words: number;
+  sentences: number;
+}
+
+export interface IReadingStore {
+  ease: IEase;
+  setEase: (ease: IEase) => void;
+}
+
+export const useReadingStore = create<IReadingStore>((set) => ({
+  ease: {
+    index: 0,
+    syllables: 0,
+    words: 0,
+    sentences: 0,
+  },
+  setEase: (ease) => set({ ease }),
+}));
