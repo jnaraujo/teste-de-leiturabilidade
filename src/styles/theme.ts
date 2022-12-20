164f59794bb0086e65eba9d47c914b0925363b3f
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-12-20 10:23:38 -0300

    💄 style: update theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,30 +1,12 @@
 import { DefaultTheme } from "styled-components";
 
-// primary: {
-//   purple: "#7B61FF",
-// },
-// gray: {
-//   black: "#1a1a1a",
-//   white: "#fafafa",
-
-//   gray1: "#cccccc",
-//   gray2: "#6c757d",
-//   gray3: "#343a40",
-// },
-// semantic: {
-//   red: "#ef233c",
-//   red2: "#d90429",
-//   green: "#00be8b",
-//   blue: "#007bff",
-// },
-
 const lightTheme: DefaultTheme = {
   colors: {
     primary: "#1a1a1a",
     onPrimary: "#fafafa",
 
     secondary: "#7B61FF",
-    onSecondary: "#fafafa",
+    onSecondary: "#cccccc",
 
     surface: "#212529",
     onSurface: "#fafafa",
