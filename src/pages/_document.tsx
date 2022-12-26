e4b7badf8808159a6780e3251e21dfc74492391b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-12-26 11:21:11 -0300

    ✨ feat: removed clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,19 +44,6 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
-          `,
-            }}
-          />
           <meta name="theme-color" content="#317EFB" />
         </Head>
         <body>
