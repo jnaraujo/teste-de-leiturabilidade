eeed98cc64964f4a319024623d23e35736f0805b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2022-12-27 09:45:25 -0300

    ✨ feat: add lang to html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-BR">
         <Head>
           <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
 
