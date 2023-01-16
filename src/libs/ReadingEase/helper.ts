b1cfcea015c2750d93b572e6dc2eb328de2ea970
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-01-16 16:39:58 -0300

    ♻️ refactor: reading ease lib

--- src/libs/ReadingEase/helper.ts
+++ src/libs/ReadingEase/helper.ts
@@ -30,6 +30,13 @@ export function countSyllables(words: string[]) {
   return totalSyllables;
 }
 
+/**
+ * Calculate the Flesch Ease score for a given text
+ * @param words number of words in the text
+ * @param sentences number of sentences in the text
+ * @param syllables number of syllables in the text
+ * @return Flesch Ease score
+ */
 export function calculateFleschEase(
   words: number,
   sentences: number,
@@ -38,6 +45,10 @@ export function calculateFleschEase(
   return 248.835 - 1.015 * (words / sentences) - 84.6 * (syllables / words);
 }
 
+/**
+ * Calculate the Flesch Reading score for a given text
+ * @param Flesch Reading score
+} */
 export function calculateResult(fleschEase: number) {
   if (!fleschEase) return 0;
   const min = Math.min(fleschEase, 100);
