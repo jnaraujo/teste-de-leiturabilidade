7c4a34d58594862e74218407a0561c216febf3cf
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-02-14 12:00:59 -0300

    🐛 fix: navbar on mobile

--- src/components/Navbar/styles.ts
+++ src/components/Navbar/styles.ts
@@ -4,9 +4,10 @@ export const Container = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 100%;
 
-  max-width: 1050px;
+  width: 1050px;
+
+  max-width: 95vw;
 
   @media (min-width: ${(props) => props.theme.breakpoints.xl}) {
     max-width: 1200px;
