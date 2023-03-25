624812429897d519c35efc13d9a0c0350f5748eb
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-03-25 20:37:06 -0300

    ✨ feat: improved index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { DefaultSeo } from "next-seo";
 import { Grid } from "@mui/material";
-
 import {
   Container,
   Informations,
@@ -14,34 +12,20 @@ import Footer from "../components/Footer";
 import Editor from "../layouts/Editor";
 import HowItWorks from "../layouts/HowItWorks";
 
-const Home = () => {
+export default function Page() {
   return (
-    <>
-      <DefaultSeo
-        title="Teste de Leiturabilidade - Faça seu texto ser entendido por todos"
-        description="Saiba em tempo real e de graça o quão fácil de ser lido seu texto é. Importe arquivos do Google Docs, Notion, etc e faça o teste de leiturabilidade."
-        additionalLinkTags={[
-          {
-            rel: "icon",
-            href: "/favicon.ico",
-          },
-        ]}
-      />
-      <MainContainer>
-        <MainContent>
-          <Editor />
-          <div className="line" />
-          <Informations>
-            <HowItWorks />
-            <Container container justifyContent="center">
-              <Grid item xs={12} className="line" />
-              <Footer />
-            </Container>
-          </Informations>
-        </MainContent>
-      </MainContainer>
-    </>
+    <MainContainer>
+      <MainContent>
+        <Editor />
+        <div className="line" />
+        <Informations>
+          <HowItWorks />
+          <Container container justifyContent="center">
+            <Grid item xs={12} className="line" />
+            <Footer />
+          </Container>
+        </Informations>
+      </MainContent>
+    </MainContainer>
   );
 };
-
-export default Home;
