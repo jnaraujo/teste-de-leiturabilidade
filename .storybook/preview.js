87c45e8d1806c5d82b70ef054550e7f98bb995e4
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-05 12:14:20 -0300

    ✨ feat: removed global styled files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,18 +1,8 @@
 import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
 import { addDecorator } from "@storybook/react";
-import { withThemesProvider } from "storybook-addon-styled-component-theme";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "../src/styles/theme";
-import { GlobalStyles } from "../src/styles/global";
+import "../src/styles/globals.scss";
 
-const themes = [darkTheme, lightTheme];
-addDecorator(withThemesProvider(themes), ThemeProvider);
-addDecorator((s) => (
-  <>
-    <GlobalStyles />
-    {s()}
-  </>
-));
+addDecorator((s) => <>{s()}</>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
