36e85d1b00ac552bc925961d5a9401c94b28ab7c
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-05 14:15:58 -0300

    ✨ feat(Button): add asChild and improved style

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
 import cx from 'classnames';
+import { Slot } from '@radix-ui/react-slot';
 
 import styles from './styles.module.scss';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  asChild?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
   children,
   className,
+  asChild,
   ...rest
 }) => {
-  return <button className={cx(
+  const Comp = asChild ? Slot : 'button';
+
+  return <Comp className={cx(
     styles.button,
     className
-  )} {...rest}>{children}</button>;
+  )} {...rest}>{children}</Comp>;
 }
 
 export default Button;
