f4f46a40c9c9403d364abfb00f724b3a9f93ccf7
jonatas araujo <jonatasaraujo2257@gmail.com>
2023-05-06 17:54:16 -0300

    ✨ feat(Layout/Editor): removed mui grid

--- src/layouts/Editor/index.tsx
+++ src/layouts/Editor/index.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect } from "react";
-import { Grid } from "@mui/material";
 import cx from "classnames";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { useImportExternalPage } from "../../hooks/useImportExternalPage";
@@ -30,29 +29,28 @@ const Editor: React.FC = () => {
 
   return (
     <>
-      <Grid container justifyContent="center" className={styles.container}>
-        <Grid item xs={12} md={10} lg={8} className={
-          cx(
-            styles.content,
-            styles.left
-          )
-        }>
-          <div className={styles.textarea}>
-            <TextEditor html={pageContent} />
-          </div>
-        </Grid>
+      <div className={styles.container}>
+        <div
+          className={
+            cx(
+              styles.content,
+              styles.left,
+              styles.textarea
+            )
+          }>
+          <TextEditor html={pageContent} />
+        </div>
 
-        <Grid item xs={11} sm={8} md={10} lg={3} className={
-          cx(
-            styles.content,
-            styles.right
-          )
-        }>
-          <aside>
-            <ResultBox onImportPage={handleImportClick} />
-          </aside>
-        </Grid>
-      </Grid>
+        <aside
+          className={
+            cx(
+              styles.content,
+              styles.right
+            )
+          }>
+          <ResultBox onImportPage={handleImportClick} />
+        </aside>
+      </div>
 
       {loading && (
         <div className={styles.loading}>
