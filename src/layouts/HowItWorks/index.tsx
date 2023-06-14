169fbc9df98533b5e13fe8ae0259f8d17d526c62
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-14 20:03:55 -0300

    ✨ feat: improved HowItWorks

--- src/layouts/HowItWorks/index.tsx
+++ src/layouts/HowItWorks/index.tsx
@@ -4,9 +4,7 @@ import styles from "./styles.module.scss";
 const HowItWorks: React.FC = () => (
   <section className={styles.container}>
     <div className={styles.texts} id="como-funciona">
-      <h3>
-        Como funciona o Teste de Leiturabilidade?
-      </h3>
+      <h3>Como funciona o Teste de Leiturabilidade?</h3>
       <p>
         Para testar o nível de leitura de um texto, nós utilizamos o{" "}
         <a
@@ -26,9 +24,7 @@ const HowItWorks: React.FC = () => (
         USP de São Carlos;
       </p>
 
-      <h3>
-        O que o Teste de Leitura leva em conta?
-      </h3>
+      <h3>O que o Teste de Leitura leva em conta?</h3>
       <p>
         Tanto a versão original do <strong>Teste de Flesch-Kincaid</strong>{" "}
         quando a versão brasileira levam em conta o{" "}
@@ -37,9 +33,7 @@ const HowItWorks: React.FC = () => (
         ler.
       </p>
 
-      <h3>
-        Quão preciso é o Teste de Leitura?
-      </h3>
+      <h3>Quão preciso é o Teste de Leitura?</h3>
       <p>
         A versão original do <strong>Teste de Flesch-Kincaid</strong> tem
         precisão de quase 90% - o que, para uso comum, é um valor bastante
@@ -47,8 +41,57 @@ const HowItWorks: React.FC = () => (
       </p>
 
       <h3>
-        Quer saber mais? Dá uma olhada nas fontes:
+        Quais são as principais métricas utilizadas no Teste de Leiturabilidade?
       </h3>
+      <p>
+        No Teste de Leiturabilidade, algumas das principais métricas utilizadas
+        incluem o número médio de sílabas por palavra, o número médio de
+        palavras por frase e o índice de facilidade de leitura. Essas métricas
+        são calculadas para determinar o nível de complexidade e legibilidade de
+        um texto.
+      </p>
+
+      <h3>
+        O Teste de Leiturabilidade é amplamente utilizado na criação de conteúdo
+        online?
+      </h3>
+      <p>
+        Sim, o Teste de Leiturabilidade é amplamente utilizado na criação de
+        conteúdo online para avaliar a adequação do texto ao público-alvo. Ao
+        considerar a leiturabilidade, os escritores podem ajustar seu estilo de
+        escrita e escolher palavras e estruturas de frase mais apropriadas para
+        garantir que o texto seja compreendido de forma eficaz pelo público
+        pretendido.
+      </p>
+
+      <h3>
+        O Teste de Leiturabilidade leva em consideração a estrutura do texto?
+      </h3>
+      <p>
+        O Teste de Leiturabilidade geralmente não leva em consideração a
+        estrutura do texto. Ele se concentra principalmente em aspectos como o
+        tamanho das palavras e das frases. No entanto, a estrutura do texto,
+        como a organização das ideias, a presença de tópicos e subtópicos, e a
+        coerência e coesão do texto, também pode afetar a legibilidade e a
+        compreensão geral do leitor.
+      </p>
+
+      <h3>
+        O Teste de Leiturabilidade é igualmente eficaz para avaliar a
+        legibilidade de diferentes gêneros de texto?
+      </h3>
+      <p>
+        O Teste de Leiturabilidade pode fornecer uma indicação geral da
+        legibilidade de diferentes gêneros de texto, mas sua eficácia pode
+        variar. Alguns gêneros, como textos científicos ou técnicos, podem ter
+        terminologia específica e estruturas de frase mais complexas, o que pode
+        influenciar a legibilidade. Portanto, ao avaliar a legibilidade de
+        diferentes gêneros de texto, é importante considerar a natureza
+        específica do gênero e o público-alvo ao interpretar os resultados do
+        teste.
+      </p>
+
+      <h3>Quer saber mais? Dá uma olhada nas fontes:</h3>
       <ul>
         <li>
           <a
