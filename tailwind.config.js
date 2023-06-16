017a0225fbac3d0386d92db26e5bd9008799266e
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-06-16 17:17:53 -0300

    🔥 configure stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,13 +8,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    fontFamily: {
+      sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+      serif: ["var(--font-merriweather)", ...defaultTheme.fontFamily.serif],
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
