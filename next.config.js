90a1a60c86cba06ec04cc9db96d86274927248ff
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-08-01 12:15:15 -0300

    ✨ feat: removed sentry

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-const { withSentryConfig } = require("@sentry/nextjs");
-
 /* eslint-disable import/no-extraneous-dependencies */
 const TerserPlugin = require("terser-webpack-plugin");
 const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
@@ -61,13 +55,5 @@ const nextConfig = {
 };
 
 module.exports = withBundleAnalyzer(
-  withSentryConfig(
-    nextConfig,
-    {
-      silent: true,
-    },
-    {
-      hideSourcemaps: true,
-    }
-  )
+  nextConfig
 );
