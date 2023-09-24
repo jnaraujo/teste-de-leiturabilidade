699b8c4ad376366bb06285d44e0502a0d59b892c
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-24 11:08:12 -0300

    ✨ feat: moved editor to app dir

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,21 @@
+import Editor from "../../layouts/Editor";
+import styles from "../../styles/pages/Editor.module.scss";
+
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Editor de Facilidade de Leitura - Teste de Leiturabilidade',
+  description: 'Editor completo com teste de leitura integrado. Saiba em tempo real e de graça o quão fácil de ser lido seu texto é. Importa arquivos do Google Docs, Notion, etc e faça o teste de Leiturabilidade.',
+}
+
+export default function Page() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <Editor />
+        </div>
+      </div>
+    </>
+  );
+};
