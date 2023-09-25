b590cede7c21726101c4b0cb9587de058f917522
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-25 00:08:35 -0300

    🔥 improve blog cache i guess

--- src/services/BlogService.tsx
+++ src/services/BlogService.tsx
@@ -63,12 +63,23 @@ export async function fetchPost(id: string) {
   return post;
 }
 
+interface PostWithHtml extends Post {
+  html: string;
+  readingTime: number;
+}
+
+const cache = new Map<string, PostWithHtml>();
+
 export async function fetchPostBySlug(slug: string) {
   const posts = await fetchPosts(Infinity);
 
   const post = posts.find((post) => slugfy(post.Title) === slug);
 
   if (!post) return null;
+
+  if(cache.has(post.id)) {    
+    return cache.get(post.id);
+  }
 
   const blockMap = await fetchPost(post.id);
 
@@ -79,10 +90,13 @@ export async function fetchPostBySlug(slug: string) {
   );
 
   const time = getReadingTime(html);
-
-  return {
+  const result = {
     ...post,
     readingTime: time,
     html,
   };
+
+  cache.set(post.id, result);
+
+  return result;
 }
