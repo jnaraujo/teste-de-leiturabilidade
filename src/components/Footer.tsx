c31f91687b6819f79062518a0b7af6a04ad53d6e
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-11 18:08:16 -0300

    🚨 run lint

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
 const Footer: React.FC = () => (
-  <footer className="text-center text-gray-500 text-sm py-4">
+  <footer className="py-4 text-center text-sm text-gray-500">
     Feito com ❤️ por{" "}
-    <Link href="https://jnaraujo.com/" target="_blank" className="text-violet-600 hover:text-violet-700">
+    <Link
+      href="https://jnaraujo.com/"
+      target="_blank"
+      className="text-violet-600 hover:text-violet-700"
+    >
       Jônatas Araújo
     </Link>{" "}
     - {new Date().getFullYear()}
