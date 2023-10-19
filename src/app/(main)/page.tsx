84451dab9917202f74a9c936be7f23f67d9ebd9e
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-18 23:11:26 -0300

    🔥 chore: removed dead code

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,13 +6,11 @@ import HowItWorks from "@/components/layouts/HowItWorks";
 
 export default function Page() {
   return (
-    <>
-      <article className="container">
-        <Editor />
-        <div className="h-[1px] w-full max-w-5xl bg-gray-300" />
-        <HowItWorks />
-      </article>
-    </>
+    <article className="container">
+      <Editor />
+      <div className="h-[1px] w-full max-w-5xl bg-gray-300" />
+      <HowItWorks />
+    </article>
   );
 }
 
