951171a6422102e56aa3e6c3e9ac985cc25bc374
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-19 19:47:06 -0300

    🐛 fix: editor grid

--- src/components/layouts/ProEditor/ProEditorLayout.tsx
+++ src/components/layouts/ProEditor/ProEditorLayout.tsx
@@ -9,7 +9,7 @@ const ProEditor = dynamic(() => import("@/components/Editors/Editor"), {
 
 export default function ProEditorLayout() {
   return (
-    <section className="container grid grid-cols-1 gap-4 sm:grid-cols-[1fr_300px]">
+    <section className="container grid grid-cols-1 gap-4 md:grid-cols-[minmax(450px,1fr)_250px] lg:grid-cols-[1fr_300px]">
       <div className="mx-auto min-h-[100svh] w-full">
         <div className="h-full w-full max-w-3xl">
           <ProEditor isPro={true} />
