040cceb144db5bd27c33e0c3f799d2f7a49fb413
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-06 18:03:29 -0300

    ✨ feat: add dark mode to text editor layout

--- src/components/layouts/Editor/index.tsx
+++ src/components/layouts/Editor/index.tsx
@@ -10,7 +10,7 @@ const Editor: React.FC = () => {
   return (
     <section className="grid grid-cols-1 gap-4 md:grid-cols-[minmax(450px,1fr)_250px] lg:grid-cols-[1fr_300px]">
       <div className="mx-auto mb-6 min-h-[100svh] w-full rounded-lg">
-        <div className="h-full w-full max-w-3xl overflow-hidden rounded-lg border border-zinc-300 bg-stone-200/50 shadow-md">
+        <div className="h-full w-full max-w-3xl overflow-hidden rounded-lg border border-zinc-300 bg-stone-200/50 shadow-md dark:border-zinc-600 dark:bg-transparent">
           <TextEditor />
         </div>
       </div>
