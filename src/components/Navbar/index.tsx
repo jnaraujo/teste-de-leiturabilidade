1a5b34539aad8eb26e477541965163a2d6b20389
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-06 18:18:31 -0300

    ♻️ refactor: navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,8 +2,10 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { Links } from "./Links";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { NAVBAR_LINKS } from "./constants";
+import { Button } from "../ui/button";
+import { cn } from "@/libs/utils";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +23,64 @@ const Navbar = () => {
       >
         Teste de Leiturabilidade
       </Link>
-      <Links onClose={handleClick} isOpen={isOpen} />
+
+      <nav className="hidden sm:block">
+        <ul className="flex items-center justify-center gap-4">
+          {NAVBAR_LINKS.map(({ title, url }) => (
+            <li key={title}>
+              <Link href={url} className="text-zinc-700 hover:text-zinc-800">
+                {title}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <Button
+              asChild
+              className="h-full bg-violet-600 hover:bg-violet-700"
+            >
+              <Link href="/editor">Testar o Novo Editor!</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+
+      <nav
+        className={cn(
+          "hidden bg-zinc-100 p-4 dark:bg-zinc-900",
+          "fixed bottom-0 left-0 right-0 top-0 z-50", // position
+          {
+            "flex items-center justify-center sm:hidden": isOpen,
+          },
+        )}
+      >
+        <button
+          className="absolute right-2 top-2 text-2xl text-zinc-700"
+          type="button"
+          onClick={handleClick}
+          aria-label="Abrir menu"
+        >
+          <AiOutlineClose />
+        </button>
+
+        <ul className="flex flex-col items-center justify-center gap-4">
+          {NAVBAR_LINKS.map(({ title, url }) => (
+            <li key={title}>
+              <Link href={url} className="text-zinc-700 hover:text-zinc-800">
+                {title}
+              </Link>
+            </li>
+          ))}
+          <li>
+            <Button
+              asChild
+              className="h-full bg-violet-600 hover:bg-violet-700"
+            >
+              <Link href="/editor">Testar o Novo Editor!</Link>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+
       <button
         className="text-2xl text-zinc-700 focus:outline-none sm:hidden"
         type="button"
