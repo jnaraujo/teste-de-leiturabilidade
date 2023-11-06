bfba5c90ae354a8a987ff153482bcc304cd61a4c
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-06 19:36:04 -0300

    ✨ feat: add color variables to global styles and import utility function in Editor component.

--- src/components/Editors/Editor/index.tsx
+++ src/components/Editors/Editor/index.tsx
@@ -12,6 +12,7 @@ import { useConfigStore } from "@/store/configStore";
 import clsx from "clsx";
 import { useStatsStore } from "@/store/statsStore";
 import { BubbleMenu } from "../shared/BubbleMenu";
+import { cn } from "@/libs/utils";
 import Toolbar from "../ProEditor/Toolbar";
 
 type ComponentPropsType = {
