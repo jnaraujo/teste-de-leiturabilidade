d267bb94fa9e4cc30f1b14903ee09c09d0c1d142
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-06 13:35:04 -0300

    ✨ feat: add ThemeProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,23 +3,26 @@
 import { ModalProvider } from "@/context/ModalContext";
 import { Toaster } from "react-hot-toast";
 import { TooltipProvider } from "./ui/tooltip";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TooltipProvider delayDuration={250}>
-      <ModalProvider>
-        {children}
+    <ThemeProvider>
+      <TooltipProvider delayDuration={250}>
+        <ModalProvider>
+          {children}
 
-        <Toaster
-          toastOptions={{
-            duration: 5000,
-            style: {
-              fontFamily: "var(--font-inter)",
-            },
-          }}
-          position="top-center"
-        />
-      </ModalProvider>
-    </TooltipProvider>
+          <Toaster
+            toastOptions={{
+              duration: 5000,
+              style: {
+                fontFamily: "var(--font-inter)",
+              },
+            }}
+            position="top-center"
+          />
+        </ModalProvider>
+      </TooltipProvider>
+    </ThemeProvider>
   );
 }
---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+interface IThemeContext {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+}
+
+const ThemeContext = createContext<IThemeContext>({} as IThemeContext);
+
+export const useTheme = () => useContext(ThemeContext);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (typeof window !== "undefined") {
+      const theme = localStorage.getItem("theme") as Theme;
+      if (theme) {
+        if (theme === "dark") {
+          document.documentElement.classList.add("dark");
+        }
+
+        return theme;
+      }
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
+
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
