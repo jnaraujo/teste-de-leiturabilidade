b79ad19c6e0dc83e1c831e5484c6ff41b55b9bef
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-02 11:35:22 -0300

    ✨ feat(Feedback Modal): replace scss to tailwind

--- src/components/FeedbackWidget/index.tsx
+++ src/components/FeedbackWidget/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import styles from "./styles.module.scss";
 import dynamic from "next/dynamic";
 import React, { useState, useEffect } from "react";
 const FeedbackModal = dynamic(() => import("./FeedbackModal"));
@@ -22,11 +21,14 @@ const FeedbackWidget: React.FC = () => {
     <>
       <Tooltip text="Deixe seu Feedback!" placement="top">
         <button
-          className={styles.widget}
           onClick={handleModal}
           aria-label="Deixe seu Feedback!"
+          className="group absolute bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full bg-violet-500 transition-all duration-200 hover:scale-110 hover:bg-violet-600"
         >
-          <MessageSquarePlus />
+          <MessageSquarePlus
+            className="text-zinc-50 transition-transform duration-200 group-hover:scale-[0.85]"
+            size={26}
+          />
         </button>
       </Tooltip>
       {openModal && <FeedbackModal onClose={handleModal} open={openModal} />}
