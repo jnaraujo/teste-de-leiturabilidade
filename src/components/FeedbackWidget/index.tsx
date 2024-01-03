cdebf8775cd7055024142598856e425731e79992
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-02 21:45:29 -0300

    🐛 fix: feedback widget position

--- src/components/FeedbackWidget/index.tsx
+++ src/components/FeedbackWidget/index.tsx
@@ -23,7 +23,7 @@ const FeedbackWidget: React.FC = () => {
         <button
           onClick={handleModal}
           aria-label="Deixe seu Feedback!"
-          className="group absolute bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full bg-violet-500 transition-all duration-200 hover:scale-110 hover:bg-violet-600"
+          className="group fixed bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full bg-violet-500 transition-all duration-200 hover:scale-110 hover:bg-violet-600"
         >
           <MessageSquarePlus
             className="text-zinc-50 transition-transform duration-200 group-hover:scale-[0.85]"
