6ff9e9e03ebe262a493f3bed69975bbaaa2b7f8a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-03-23 10:59:07 -0300

    🔥 chore: reduce sentry bundle size

--- next.config.js
+++ next.config.js
@@ -18,6 +18,20 @@ const nextConfig = {
       @import "./src/styles/variables.scss";
     `,
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __SENTRY_DEBUG__: false,
+        __SENTRY_TRACING__: false,
+        __RRWEB_EXCLUDE_IFRAME__: true,
+        __RRWEB_EXCLUDE_SHADOW_DOM__: true,
+        __SENTRY_EXCLUDE_REPLAY_WORKER__: true,
+      }),
+    );
+
+    // return the modified config
+    return config;
+  },
   pageExtensions: ["mdx", "jsx", "js", "ts", "tsx"],
   eslint: {
     ignoreDuringBuilds: true,
