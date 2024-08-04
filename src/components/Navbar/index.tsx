c5630b027ac2ab2900255e672ec4a83f8d1e65c2
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-08-04 16:49:39 -0300

    feat: Update Navbar component to improve accessibility and styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,48 +9,66 @@ import ThemeToggle from "../ThemeToggle";
 import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
+  return (
+    <header className="container flex items-center justify-between py-4">
+      <Link
+        href="/"
+        passHref
+        className="text-lg font-semibold text-zinc-700 dark:text-stone-200"
+      >
+        Teste de Leiturabilidade
+      </Link>
+
+      <Nav />
+      <MobileNav />
+    </header>
+  );
+};
+export default Navbar;
+
+function Nav() {
+  return (
+    <nav className="hidden sm:block">
+      <ul className="flex items-center justify-center gap-4">
+        {NAVBAR_LINKS.map(({ title, url }) => (
+          <li key={title}>
+            <Link
+              href={url}
+              className="text-zinc-700 hover:text-zinc-800 dark:text-stone-400 dark:hover:text-stone-500"
+            >
+              {title}
+            </Link>
+          </li>
+        ))}
+        <li>
+          <Button
+            asChild
+            className="h-full bg-violet-600 hover:bg-violet-700 dark:text-stone-200"
+          >
+            <Link href="/editor">Testar o Novo Editor!</Link>
+          </Button>
+        </li>
+        <li>
+          <ThemeToggle />
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
+  function closeMenu() {
+    setIsOpen(false);
+  }
+
   return (
-    <header className="container flex items-center justify-between py-4">
-      <Link
-        href="/"
-        passHref
-        className="text-lg font-semibold text-zinc-700 dark:text-zinc-300"
-      >
-        Teste de Leiturabilidade
-      </Link>
-
-      <nav className="hidden sm:block">
-        <ul className="flex items-center justify-center gap-4">
-          {NAVBAR_LINKS.map(({ title, url }) => (
-            <li key={title}>
-              <Link
-                href={url}
-                className="text-zinc-700 hover:text-zinc-800 dark:text-zinc-400 dark:hover:text-zinc-500"
-              >
-                {title}
-              </Link>
-            </li>
-          ))}
-          <li>
-            <Button
-              asChild
-              className="h-full bg-violet-600 hover:bg-violet-700 dark:text-zinc-200"
-            >
-              <Link href="/editor">Testar o Novo Editor!</Link>
-            </Button>
-          </li>
-          <li>
-            <ThemeToggle />
-          </li>
-        </ul>
-      </nav>
-
+    <>
       <nav
         className={cn(
           "hidden bg-zinc-100 p-4 dark:bg-zinc-900",
@@ -74,7 +92,8 @@ const Navbar = () => {
             <li key={title}>
               <Link
                 href={url}
-                className="text-zinc-700 hover:text-zinc-800 dark:text-zinc-400"
+                className="text-zinc-700 hover:text-zinc-800 dark:text-stone-400 dark:hover:text-stone-500"
+                onClick={closeMenu}
               >
                 {title}
               </Link>
@@ -83,7 +102,7 @@ const Navbar = () => {
           <li>
             <Button
               asChild
-              className="h-full bg-violet-600 hover:bg-violet-700 dark:text-zinc-200"
+              className="h-full bg-violet-600 text-zinc-50 hover:bg-violet-700"
             >
               <Link href="/editor">Testar o Novo Editor!</Link>
             </Button>
@@ -93,7 +112,6 @@ const Navbar = () => {
           </li>
         </ul>
       </nav>
-
       <button
         className="text-2xl text-zinc-700 focus:outline-none sm:hidden"
         type="button"
@@ -102,7 +120,6 @@ const Navbar = () => {
       >
         <Menu />
       </button>
-    </header>
+    </>
   );
-};
-export default Navbar;
+}
