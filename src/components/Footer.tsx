948defbcb882d17006b91a6e91a6a2f7e29982e0
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-08-04 13:30:49 -0300

    feat: Update Footer component to improve layout and add useful links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,48 @@
 import Link from "next/link";
 
 const Footer: React.FC = () => (
-  <footer className="py-4 text-center text-sm text-gray-500">
-    Feito com ❤️ por{" "}
-    <Link
-      href="https://jnaraujo.com/"
-      target="_blank"
-      className="text-violet-600 underline hover:text-violet-700"
-    >
-      Jônatas Araújo
-    </Link>{" "}
-    - {new Date().getFullYear()}
+  <footer className="container grid grid-cols-1 gap-4 py-4 text-center text-gray-500 md:grid-cols-2 md:text-start">
+    <p className="text-sm text-zinc-500 dark:text-stone-400">
+      Feito com ❤️ por{" "}
+      <Link
+        href="https://jnaraujo.com/"
+        target="_blank"
+        className="text-violet-600 underline hover:text-violet-700 dark:text-violet-400"
+      >
+        Jônatas Araújo
+      </Link>{" "}
+      - {new Date().getFullYear()}
+    </p>
+
+    <div className="md:justify-self-end">
+      <h5 className="text-lg font-semibold text-zinc-700">Links úteis:</h5>
+      <ul className="space-y-1">
+        <li>
+          <a
+            className="text-sm text-violet-600 underline hover:text-violet-700 dark:text-violet-400"
+            href="/contador-de-palavras"
+          >
+            Contador de Palavras
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-sm text-violet-600 underline hover:text-violet-700 dark:text-violet-400"
+            href="/editor"
+          >
+            Editor de Leiturabilidade
+          </a>
+        </li>
+        <li>
+          <a
+            className="text-sm text-violet-600 underline hover:text-violet-700 dark:text-violet-400"
+            href="/blog"
+          >
+            Blog
+          </a>
+        </li>
+      </ul>
+    </div>
   </footer>
 );
 
