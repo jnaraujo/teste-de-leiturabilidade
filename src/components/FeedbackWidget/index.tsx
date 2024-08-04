314cd699d865b1276d175c9e7289e2b739649a6c
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-08-04 13:46:07 -0300

    fix: tooltip oon feedback modal

--- src/components/FeedbackWidget/index.tsx
+++ src/components/FeedbackWidget/index.tsx
@@ -1,24 +1,28 @@
 "use client";
 import React from "react";
-import Tooltip from "../Tooltip";
 import { MessageSquarePlus } from "lucide-react";
 import FeedbackModal from "./FeedbackModal";
+import { Tooltip, TooltipContent, TooltipProvider } from "../ui/tooltip";
+import { TooltipTrigger } from "@radix-ui/react-tooltip";
 
 const FeedbackWidget: React.FC = () => {
   return (
     <>
-      <Tooltip text="Deixe seu Feedback!" placement="top">
+      <Tooltip>
         <FeedbackModal>
-          <button
-            aria-label="Deixe seu Feedback!"
-            className="group fixed bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full bg-violet-500 transition-all duration-200 hover:scale-110 hover:bg-violet-600"
-          >
-            <MessageSquarePlus
-              className="text-zinc-50 transition-transform duration-200 group-hover:scale-[0.85]"
-              size={26}
-            />
-          </button>
+          <TooltipTrigger asChild>
+            <button
+              aria-label="Deixe seu Feedback!"
+              className="group fixed bottom-4 right-4 flex h-14 w-14 items-center justify-center rounded-full bg-violet-500 transition-all duration-200 hover:scale-110 hover:bg-violet-600"
+            >
+              <MessageSquarePlus
+                className="text-zinc-50 transition-transform duration-200 group-hover:scale-[0.85]"
+                size={26}
+              />
+            </button>
+          </TooltipTrigger>
         </FeedbackModal>
+        <TooltipContent>Deixe seu Feedback!</TooltipContent>
       </Tooltip>
     </>
   );
