5060d68b1f6acc03d73a4a416a2885856b027a40
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-08-11 15:38:55 -0300

    feat: update blog post URLs to use the correct domain

--- src/pages/api/blog/posts.tsx
+++ src/pages/api/blog/posts.tsx
@@ -4,7 +4,7 @@ export default async function Posts(req: any, res: any) {
   const posts = await fetchPosts(Infinity);
   const urls = [];
   for (let post of posts) {
-    urls.push(`https://leiturabilidade.jnaraujo.com/blog/${post.Slug}`);
+    urls.push(`https://leiturabilidade.site/blog/${post.Slug}`);
   }
 
   res.status(200).send(urls.join("\n"));
