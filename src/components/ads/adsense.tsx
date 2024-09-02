0a0991a8e7fbbe7dde9296489dcb1b1643065a5a
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-09-02 10:05:52 -0300

    feat: Update AdSense component to use lazy loading for better performance

--- src/components/ads/adsense.tsx
+++ src/components/ads/adsense.tsx
@@ -1,7 +1,10 @@
+import Script from "next/script";
+
 export function AdSense() {
   return (
-    <script
+    <Script
       async
+      strategy="lazyOnload"
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${process.env.NEXT_PUBLIC_ADSENSE}`}
       crossOrigin="anonymous"
     />
